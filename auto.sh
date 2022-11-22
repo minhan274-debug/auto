c148ed9c13e3e0ac3a2384cd04f45d6685ca8418
Marcel <minhan04022000@gmail.com>
2022-11-22 16:22:32 +0700

    update auto

--- auto.sh
+++ auto.sh
@@ -1,19 +1,28 @@
-user=$HOME
-base_dir=$user/Desktop/ruby-project;
-config_dir=$user/Desktop/devops_config;
+user=$HOME;
+devops=$user/devops;
+base_dir=$devops/ruby-project;
+config_dir=$devops/auto;
 fe_dir=$base_dir/intern-fe-2022;
 be_dir=$base_dir/intern-be-2022;
 
 #stop current container
 cd $base_dir
 docker compose down
+#handle file names 
+cd $devops
+count=`cat count.txt`
+#backup old_file
+if [ -d "$base_dir" ]; then
+	sudo mv $base_dir $base_dir$count 
+fi
+#
 cd $config_dir
 # set-up for frontend
 if [ -d "$fe_dir" ]; then
 	sudo rm -r $fe_dir
 fi
 git clone --branch develop git@github.com:autuanthinh/intern-fe-2022.git $fe_dir
-#git clone --branch develop https://github.com/autuanthinh/intern-fe-2022.git
+cp ./Dockerfile-fe $fe_dir/Dockerfile
 rm $fe_dir/env/.env.development.js
 cp ./.env.development.js $fe_dir/env/.env.development.js
 
@@ -22,13 +31,13 @@ if [ -d "$be_dir" ]; then
         sudo rm -r $be_dir
 fi
 git clone --branch devops git@github.com:autuanthinh/intern-be-2022.git $be_dir
-#git clone --branch devops https://github.com/autuanthinh/intern-be-2022.git
 cp ./Dockerfile-be $be_dir/Dockerfile
 
 #config database.yml 
 cp ./database.yml $be_dir/config
 #docker-compose
 cp ./docker-compose.yml $base_dir
+#check project exist
 
 #run
 cd $base_dir	
